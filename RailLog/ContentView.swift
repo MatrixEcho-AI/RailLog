cf4bf50e8cda09b2e8700803cc6ad57b309440ed
MikeWu597 <i@hyp.ink>
2026-05-05 01:01:28 +0800

    Initial Commit

---
+++ RailLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RailLog
+//
+//  Created by MikeWu597 on 2026/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RailLog/RailLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailLogApp.swift
+//  RailLog
+//
+//  Created by MikeWu597 on 2026/5/5.
+//
+
+import SwiftUI
+
+@main
+struct RailLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
